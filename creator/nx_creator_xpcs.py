af2278ab6c2693df73aa5668232be63d16c51dec
Juliane Reinhardt <jreinhardt@lbl.gov>
2021-02-10 12:06:27 -1000

    continue with NX creator

--- creator/nx_creator_xpcs.py
+++ creator/nx_creator_xpcs.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 
-# import datetime
-import h5py
-import logging
-
-# import numpy as np
-# import os
-
 logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 
 A_KEV = 12.3984244  # voltage * wavelength product
-NX_EXTENSION = (
-    ".nxs"  # .nxs is known by PyMCA, .nx is not (use generic filter then)
-)
+# .nxs is known by PyMCA, .nx is not (use generic filter then)
+NX_EXTENSION = ".nxs"
 
 NX_APP_DEF_NAME = "NXxpcs"  # name of NeXus Application Definition
 # TODO: not defined in NeXus at this time
@@ -37,34 +29,32 @@ class NXCreator:
     def __init__(self, h5root):
         self._h5root = h5root
 
-    def _init_group(self, h5parent, nm, NX_class, md=None):
+    def _init_group(self, h5parent, nm, NX_class):
         """Common steps to initialize a NeXus HDF5 group."""
-        if md is None:
-            md = {}
-
         group = h5parent.create_group(nm)
         group.attrs["NX_class"] = NX_class
-        return group, md
+        return group
+
+    def _init_md(self, md=None):
+        if md is None:
+            md = {}
+        return md
 
     def create_entry_group(self, md=None, count_entry=None):
         """
         all information about the measurement
-
         see: https://manual.nexusformat.org/classes/base_classes/NXentry.html
         """
-        entry_name = "entry"
 
         if count_entry is None:
             entry_name = "entry"
         else:
+            #TODO how will count_entry be defined when connecting to the loader?
             entry_name = f"entry_{count_entry}"
-        group, md = self._init_group(
-            self._h5root, entry_name, "NXentry", md
-        )
-        # print('group', group, 'md', md)
+        group = self._init_group(self._h5root, entry_name, "NXentry", md)
+        md = self._init_md(md)
 
         group.create_dataset("definition", data=NX_APP_DEF_NAME)
-
         experiment_description = md.get("experiment_description")
         if experiment_description is not None:
             group.create_dataset(
@@ -81,35 +71,59 @@ class NXCreator:
 
         return group
 
-    def create_instrument_group(self, h5parent, md=None, count_entry=None):
-        """Write the NXinstrument group."""
+    def create_xpcs_data_group(self, h5parent, md=None, count_entry=None):
+        group, md = self._init_group(h5parent, "data", "NXsubentry", md)
 
-        if "instrument" not in md:
-            return
-        
-        # TODO: will need to get and add data
-        group, md = self._init_group(
-            h5parent, "instrument", "NXinstrument", md
-        )
+        ds = group.create_dataset("g2", data=md["XPCS"]["g2"])
+        ds.attrs["units"] = "a.u."
+        ds.attrs["target"] = ds.name
 
-        name_field = md["instrument"]["name"]
-        ds = group.create_dataset("name", data=name_field)
-        ds.attrs["target"] = ds.name  # we'll re-use this
-        logger.debug("instrument: %s", name_field)
+        ds = group.create_dataset("g2_stderr", data=md["XPCS"]["g2_stderr"])
+        ds.attrs["units"] = "a.u."
+        ds.attrs["target"] = ds.name
 
-        # self._create_beam_group(group, "beam", md=md, count_entry=count_entry)
-        # self._create_detector_group(
-        #     group, "detector_1", md=md, count_entry=count_entry
-        # )
-        # TODO allow to add different detector group data
-        # self.create_detector_group(group, "detector_2", md=md)
-        # self._create_monitor_group(group, "monitor", md=md)
+        ds = group.create_dataset("tau", data=md["XPCS"]["tau"])
+        ds.attrs["units"] = "a.u."
+        ds.attrs["target"] = ds.name
+
+    def _create_dataset(self, h5parent, name, md_path, **kwargs):
+        """
+        use this to create datasets in different (sub-)groups
+        """
+        ds = h5parent.create_dataset(name, data=self.md[md_path])
+        for k, v in kwargs.items():
+            ds.attrs[k] = v
+        # ds.attrs["units"] = **kwargs.items()  # FIXME allow for other units
+        ds.attrs["target"] = ds.name
+        return ds
 
     def create_xpcs_group(self, h5parent, md=None, *args, **kwargs):
         """
         see Data Solutions Pilot Meeting Notes
         """
-        group, md = self._init_group(h5parent, "XPCS", "NXsubentry", md)
+        md = self._init_md(md)
+        xpcs_group = self._init_group(h5parent, "XPCS", "NXprocess")
+        #this defines the preferred plot data
+        xpcs_group.attrs["default"] = "data"
+
+        data_group = self._init_group(xpcs_group, "data", "NXdata")
+        twotime_group = self._init_group(xpcs_group, "twotime", "NXdata")
+
+        #FIXME add correct data path and units based on loader structure
+        self._create_dataset(data_group, "g2", "md_path", units="au")
+        self._create_dataset(data_group, "g2_stderr", "md_path", units="au")
+        self._create_dataset(data_group, "tau", "md_path", units="au")
+
+        self._create_dataset(twotime_group, "g2_partials_twotime", "md_path", units="au")
+        self._create_dataset(twotime_group, "g2_twotime", "md_path", units="au")
+
+        #TODO: work out how to name this and iterate through the datasets
+        # will it be n datasets or an n dimensional array
+        # 
+        # for n in number_of_entries_from_dict:
+        #     add _create_dataset
+        self._create_dataset(twotime_group, "C_0000X", "md_path", units="au")
+
 
     def create_saxs_1d_group(self, h5parent, md=None, *args, **kwargs):
         """
@@ -138,5 +152,25 @@ class NXCreator:
         ds.attrs["units"] = "counts"
         ds.attrs["target"] = ds.name
 
-    
 
+
+
+    def create_instrument_group(self, h5parent, md=None, count_entry=None):
+        """Write the NXinstrument group."""
+
+        if "instrument" not in md:
+            return
+
+        # TODO: will need to get and add data
+        group, md = self._init_group(
+            h5parent, "instrument", "NXinstrument", md
+        )
+
+        name_field = md["instrument"]["name"]
+        ds = group.create_dataset("name", data=name_field)
+        ds.attrs["target"] = ds.name  # we'll re-use this
+        logger.debug("instrument: %s", name_field)
+
+        # TODO allow to add different detector group data
+        # self.create_detector_group(group, "detector_2", md=md)
+        # self._create_monitor_group(group, "monitor", md=md)
