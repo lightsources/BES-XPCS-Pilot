3dbe2625e6f20ed1a9f6a0c9376633e8965e06e2
Dylan McReynolds <dmcreynolds@lbl.gov>
2021-02-10 15:04:53 -0800

    checkpoint test

--- tests/test_nx_create.py
+++ tests/test_nx_create.py
@@ -4,18 +4,15 @@ from creator.nx_creator_xpcs import NXCreator
 def test_nx_xpcs(tmp_path):
     with  h5py.File(tmp_path / 'test.hdf5', 'w') as file:
         try:
-            md = {
-                "instrument": {
-                    "name": "beamline"
-
-                }
-            }
+            
 
             creator = NXCreator(file)
             group = creator.create_entry_group(md=md)
-            creator.create_instrument_group(group, md=md)
+
+            
+            creator.create_instrument_group(group, md=instrument_md)
             creator.create_xpcs_group(group, md=md)
-            creator.create_saxs_1d_group(group, md=md)
+            creator.create_saxs_1d_group(group, md=saxs_1d_md)
             creator.create_saxs_2d_group(group, md=md)
         except Exception as e:
             raise e
@@ -25,4 +22,5 @@ def test_nx_xpcs(tmp_path):
         assert '/entry/instrument/name' in file
         assert file['/entry/instrument/name'][()] == "beamline"
     
-    
+instrument_md = {"instrument": {"name": "beamline"}}
+saxs_1d_md = {"SAXS_1D": {"name": "beamline"}}
